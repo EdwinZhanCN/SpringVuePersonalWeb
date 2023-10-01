55e86ab4d9f59676490e257d1f546c993340f727
EdwinZhan <108310800+EdwinZhanCN@users.noreply.github.com>
2023-09-30 23:39:06 -0400

    update

---
+++ personalWeb/personal-web/src/main/java/online/edwinzhan/personalweb/PersonalWebApplication.java
@@ -0,0 +1,13 @@
+package online.edwinzhan.personalweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalWebApplication.class, args);
+    }
+
+}
---
+++ personalWeb/personal-web/src/test/java/online/edwinzhan/personalweb/PersonalWebApplicationTests.java
@@ -0,0 +1,13 @@
+package online.edwinzhan.personalweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
